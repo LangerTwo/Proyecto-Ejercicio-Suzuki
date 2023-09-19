c3764875d336786ade1f7eb9d496359a45053191
= <adrangel95@gmail.com>
2023-09-19 18:43:28 -0500

    el evento del mouse ya se coloco en modelos y posventa falta mejorarlo

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ let btnCont1 = document.querySelector('#btn-1');
 let contList1 = document.querySelector('#contenedor-1');
 let btnCont2 = document.querySelector('#btn-2');
 let contList2 = document.querySelector('#contenedor-2');
+let btnCont3 = document.querySelector('#btn-3');
+let contList3 = document.querySelector('#contenedor-3');
+let btnCont4 = document.querySelector('#btn-4');
+let contList4 = document.querySelector('#contenedor-4');
+let subMenuModel = document.querySelector('#sub-menu-model');
 
 //.addEventListener('click', );
 iconHamb.addEventListener('click', openMenuMobile);
@@ -19,6 +24,8 @@ posventaClick.addEventListener('click', openSubPos);
 
 btnCont1.addEventListener('click', chageBtn1);
 btnCont2.addEventListener('click', chageBtn2);
+btnCont3.addEventListener('click', chageBtn3);
+btnCont4.addEventListener('click', chageBtn4);
 
 function openMenuMobile() {
     navbarHeight.classList.remove('altura');
@@ -48,13 +55,40 @@ function closeSubPos() {
 
 function chageBtn1() {
     console.log("caja 1");
-    contList1.classList.remove('inactive');
-    contList2.classList.add('inactive');
+    contList1.setAttribute('class', 'container-grid');
+    contList2.setAttribute('class', 'inactive');
+    contList3.setAttribute('class', 'inactive');
+    contList4.setAttribute('class', 'inactive');
 }
 function chageBtn2() {
     console.log("caja 2");
-    contList2.classList.remove('inactive');
-    contList1.classList.add('inactive');
+    contList2.setAttribute('class', 'container-grid');
+    contList1.setAttribute('class', 'inactive');
+    contList3.setAttribute('class', 'inactive');
+    contList4.setAttribute('class', 'inactive');
+}
+function chageBtn3() {
+    console.log("caja 3");
+    contList3.setAttribute('class', 'container-grid');
+    contList1.setAttribute('class', 'inactive');
+    contList2.setAttribute('class', 'inactive');
+    contList4.setAttribute('class', 'inactive');
+}
+function chageBtn4() {
+    console.log("caja 4");
+    contList4.setAttribute('class', 'container-grid');
+    contList1.setAttribute('class', 'inactive');
+    contList2.setAttribute('class', 'inactive');
+    contList3.setAttribute('class', 'inactive');
+}
+
+function openSubModel() {
+    subMenuModel.classList.remove('inactive');
+    console.log("abre");
+}
+function closeSubModel() {
+    subMenuModel.classList.add('inactive');
+    console.log("cierra");
 }
 
 var acc = document.getElementsByClassName("acordionfooter")
